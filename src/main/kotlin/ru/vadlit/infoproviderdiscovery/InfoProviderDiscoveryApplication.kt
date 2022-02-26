742cbce7a73861db0bc669fa548c5277631cdb2d
Vadim Litvinov <vadlit@vadlit.ru>
2022-02-26 15:34:47 +0300

    first commit

---
+++ src/main/kotlin/ru/vadlit/infoproviderdiscovery/InfoProviderDiscoveryApplication.kt
@@ -0,0 +1,11 @@
+package ru.vadlit.infoproviderdiscovery
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InfoProviderDiscoveryApplication
+
+fun main(args: Array<String>) {
+    runApplication<InfoProviderDiscoveryApplication>(*args)
+}
