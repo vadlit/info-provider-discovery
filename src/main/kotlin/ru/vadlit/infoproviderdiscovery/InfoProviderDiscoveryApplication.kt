29c659f5741cdb9171906a7d1dda32b91b50b802
Vadim Litvinov <vadlit@vadlit.ru>
2022-02-26 23:12:54 +0300

    eureka server

--- src/main/kotlin/ru/vadlit/infoproviderdiscovery/InfoProviderDiscoveryApplication.kt
+++ src/main/kotlin/ru/vadlit/infoproviderdiscovery/InfoProviderDiscoveryApplication.kt
@@ -2,8 +2,10 @@ package ru.vadlit.infoproviderdiscovery
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
 @SpringBootApplication
+@EnableEurekaServer
 class InfoProviderDiscoveryApplication
 
 fun main(args: Array<String>) {
